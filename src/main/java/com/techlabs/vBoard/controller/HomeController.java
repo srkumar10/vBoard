3c6fea05ccc5397af762aa231fd08674f89eaedd
Ramesh Sunkara <ramesh.wm@gmail.com>
2015-02-08 15:35:54 -0500

    create board view sec1,2

--- src/main/java/com/techlabs/vBoard/controller/HomeController.java
+++ src/main/java/com/techlabs/vBoard/controller/HomeController.java
@@ -3,12 +3,17 @@ package com.techlabs.vBoard.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Controller
 public class HomeController {
 	
+	private static final Logger LOGGER = LoggerFactory.getLogger(HomeController.class);
+	
 	@RequestMapping(value = "/", method=RequestMethod.GET)
-	public String showHome() {
+	public String showHome() {		
+		LOGGER.debug("Rendering Home View");
 		return "home";
 	}
 
